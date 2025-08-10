b8880bcc0c8bf8ed50866e864dcfd3a96846b0f8
shivsingh01x <shivamagrawal01x@gmail.com>
2025-08-10 12:56:41 +0000

    string_indexing

---
+++ 04_String/String_indexing.js
@@ -0,0 +1,10 @@
+// Indexing in JS
+
+// Positive Indexing
+let Car = "Volvo"
+console.log("Positive Indexing");
+console.log(Car[0]);
+console.log(Car[1]);
+console.log(Car[2]);
+console.log(Car[3]);
+console.log(Car[4]);
