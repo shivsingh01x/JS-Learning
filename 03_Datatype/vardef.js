e06e06066318e125bf68ddbc3cf25ad9b6165800
shivsingh01x <shivamagrawal01x@gmail.com>
2025-08-10 12:34:29 +0000

    vardef

---
+++ 03_Datatype/vardef.js
@@ -0,0 +1,6 @@
+let way1 = "This is first way of declaring variable"
+const way2 = "This is second way of declaring variable"
+
+console.log(way1, way2);
+
+// We dont use var for declaring variable because it conflict's on block level scope
