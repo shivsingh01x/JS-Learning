e59696dc84a5903e2bd7c769b504e51fdf8bf395
shivsingh01x <shivamagrawal01x@gmail.com>
2025-08-10 11:02:35 +0000

    Variable

---
+++ 01_Basic/variable.js
@@ -0,0 +1,13 @@
+let testing = 5
+// console.log(testing);
+
+testing = 10
+// console.log(testing);
+
+const conTesting = 20
+// console.log(conTesting);
+
+// conTesting = 30
+
+// printing multiple variables
+console.log(testing, conTesting);
