99cac29c9f6609e1019fdf59e8a73d03d6069bc8
shivsingh01x <shivamagrawal01x@gmail.com>
2025-08-10 13:29:36 +0000

    string_methods

---
+++ 04_String/string_methods.js
@@ -0,0 +1,18 @@
+// String methods in js
+
+let Volvo = "xc90 "
+let oddi = "RS7 "
+console.log(Volvo.toUpperCase());
+console.log(oddi.toLowerCase());
+console.log(Volvo.slice(2)); 
+console.log(Volvo.startsWith("xc"));
+console.log(Volvo.endsWith("90"));
+console.log(Volvo.length);
+console.log(Volvo.replace("xc90", "xc60"));
+console.log(Volvo.repeat(5),"|", oddi.repeat(5));
+console.log(Volvo.search("c"));
+// console.log();
+
+
+
+
