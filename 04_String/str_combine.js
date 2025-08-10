08ada40cc7a8fc6bce64393d40627762e0c57704
shivsingh01x <shivamagrawal01x@gmail.com>
2025-08-10 12:48:18 +0000

    string Interpolation

---
+++ 04_String/str_combine.js
@@ -0,0 +1,14 @@
+// combining two string
+
+const name = "Shiv"
+const moto = "It's not over!"
+console.log(name,"has commited -", moto); //genral way of print string + var
+console.log(`Beast ${name} has commited that ${moto}`); //String Interpolation
+
+
+// another example
+
+let student = "Shiv Singh Keer"
+let marks = "96.5"
+let grade = "A+"
+console.log(`A ${student} has got ${marks} at ${grade}`);
