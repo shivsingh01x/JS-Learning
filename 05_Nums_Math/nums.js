1f223058bb041e056dc77140cc2583e109198167
Shiv Singh Keer <shivamagrawal01x@gmail.com>
2025-08-12 02:35:14 +0000

    nums.js

---
+++ 05_Nums_Math/nums.js
@@ -0,0 +1,18 @@
+// Number with js
+
+const marks = 600
+console.log(typeof(marks), marks)
+
+const balance = new Number(1000)
+console.log(typeof(balance), balance)
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(5));
+
+const prevalue = 112.456
+console.log(prevalue.toPrecision(3));
+
+const thousand = 105210548
+console.log(thousand.toLocaleString('en-in'));
+
+
