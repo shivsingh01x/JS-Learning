fd1aa2f24743b3d991ef61259f5dd02d6b8aaeac
Shiv Singh Keer <shivamagrawal01x@gmail.com>
2025-08-12 02:10:29 +0000

    let const

---
+++ 02_Let_Const/let.js
@@ -0,0 +1,10 @@
+// let const and Var
+
+let a = 5
+console.log(a);
+
+const myArr = "This is sample of const"
+console.log(myArr)
+
+// we dont use var beacuase it conflicts in block level scope
+// we will only use let and const
