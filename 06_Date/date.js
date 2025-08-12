d71ae129f15a1ab33269346d01d3d43a7711442c
Shiv Singh Keer <shivamagrawal01x@gmail.com>
2025-08-12 03:02:59 +0000

    datejs

---
+++ 06_Date/date.js
@@ -0,0 +1,16 @@
+let myDate = new Date()
+
+console.log("Simple prinitng:- ", myDate);
+console.log("toDateString:- ", myDate.toDateString());
+console.log("toTimeString:- ", myDate.toTimeString());
+console.log("toISOString:- ", myDate.toISOString());
+console.log("toLocaleDateString:- ", myDate.toLocaleDateString());
+console.log("toLocaleTimeString:- ", myDate.toLocaleTimeString());
+console.log("toString:- ", myDate.toString());
+console.log("toJSON:- ", myDate.toJSON());
+
+
+// let mycreateDate = new Date(2024, 0, 23)
+// let mycreateDate = new Date(2024, 0, 23, 5, 3, 29)
+// let mycreateDate = new Date("08-12-2025")
+// console.log(mycreateDate.toDateString());
