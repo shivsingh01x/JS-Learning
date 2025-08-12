78edab442c3a755f2a8927c09d242fdb24824886
Shiv Singh Keer <shivamagrawal01x@gmail.com>
2025-08-12 02:49:25 +0000

    math.js

---
+++ 05_Nums_Math/math.js
@@ -0,0 +1,21 @@
+// console.log(Math)
+// console.log(Math.abs(-4214));
+// console.log(Math.round(45.4));
+// console.log(Math.ceil(34.1));
+// console.log(Math.floor(34.9));
+// console.log(Math.sqrt(16));
+// console.log(Math.min(44,234,53,62));
+// console.log(Math.max(44,234,53,62));
+
+// random
+
+// console.log(Math.random()*6);
+// console.log(Math.round((Math.random()*6)));
+// console.log(Math.ceil((Math.random()*6)));
+// console.log(Math.floor((Math.random()*6)));
+
+// random fixed min max value formula
+
+const min = 1
+const max = 6
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
