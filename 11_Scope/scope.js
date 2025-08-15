1a3ae9b8ea51008bf00f45625fba8bed8a16bda9
Shiv Singh Keer <shivamagrawal01x@gmail.com>
2025-08-15 03:07:47 +0000

    scope

---
+++ 11_Scope/scope.js
@@ -0,0 +1,14 @@
+// Scope in JS
+
+if(true){
+let a = 5
+const b = 3
+var c = 2
+}
+console.log(b);
+
+// console.log(a);
+// console.log(b);
+
+// In Block scope var used to create an issue, In block scope var act as a global scope var that's why we use only let and const avoding var
+
