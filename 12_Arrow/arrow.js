3f5c8f205caec9989fd009bacc0b5c9ff6cc3327
Shiv Singh Keer <shivamagrawal01x@gmail.com>
2025-08-16 01:40:53 +0000

    arrow

---
+++ 12_Arrow/arrow.js
@@ -0,0 +1,29 @@
+// how to use this and current contest in js
+
+const user1 = {
+    username: "ShivSinghKeer",
+    website: "Youtube",
+
+    welcomeMessage: function(){
+        console.log(`${this.username}, Welcome to the ${this.website}`)
+        console.log(this)
+    }
+ 
+}
+
+// user1.welcomeMessage()
+// user1.username = ("Sammu")
+// user1.website = ("Facebook")
+// user1.welcomeMessage()
+// console.log(this)
+
+
+// we cant use this in function, used in object only
+function chai(){
+    const username = "ShivSinghKeer"
+    console.log(this.username)
+}
+
+chai()
+
+
