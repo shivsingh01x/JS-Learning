d5edbaf5ef046f4fb00129333f3c8c1ee65e0cda
Shiv Singh Keer <shivamagrawal01x@gmail.com>
2025-08-20 16:52:32 +0000

    controlFlow

---
+++ 16_ControlFlow/controlFlow.js
@@ -0,0 +1,20 @@
+// IF Statement
+
+// const IsUserLoggedIn = true
+
+// if (IsUserLoggedIn){
+//     console.log("First Programm of IF Statement")
+// }
+// else{
+//     console.log("Code Excuted")
+// }
+
+// < , > , <= , >= , == , === , && , || , != , !==
+
+if ( 1 >= 2 && 2 == 2){
+    console.log("Checking Alpha")
+}
+else{
+    console.log("Else Executed")
+}
+
