549a779abcff7bb99491b79042af91234b88240a
Shiv Singh Keer <shivamagrawal01x@gmail.com>
2025-08-21 02:22:47 +0000

    switch statement

---
+++ 16_ControlFlow/switchStatement.js
@@ -0,0 +1,42 @@
+// switch statement
+
+const month = "Feb"
+
+switch (month){
+    case "Jan":
+        console.log("January")
+        break;
+    case "Feb":
+        console.log("Feberary")
+        break;
+    case "Mar":
+        console.log("March")
+        break;
+    case "Apr":
+        console.log("Aprail")
+        break;
+    case "May":
+        console.log("May")
+        break;
+    case "Jun":
+        console.log("June")
+        break;
+    case "Jul":
+        console.log("July")
+        break;
+    case "Aug":
+        console.log("August")
+        break;
+    case "Sept":
+        console.log("September")
+        break;
+    case "Oct":
+        console.log("Octumber")
+        break;
+    case "Nov":
+        console.log("November")
+        break;
+    case "Dec":
+        console.log("December")
+        break;
+}
