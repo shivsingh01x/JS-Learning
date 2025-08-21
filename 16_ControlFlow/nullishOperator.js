f19bd38e4fb66e1b48cee3256a9aae915e595789
Shiv Singh Keer <shivamagrawal01x@gmail.com>
2025-08-21 02:54:00 +0000

    Nullish Coalescing Operator

---
+++ 16_ControlFlow/nullishOperator.js
@@ -0,0 +1,9 @@
+// Nullish Coalescing Opertor (??): null undefined
+// it is a new opertor acts as a fall back value 
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 15
+
+console.log(val1)
