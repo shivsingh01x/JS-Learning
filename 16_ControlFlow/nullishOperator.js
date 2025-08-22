ddc1d0f33b0f26690e20de59e4657f59d79f5f80
Shiv Singh Keer <shivamagrawal01x@gmail.com>
2025-08-22 02:02:12 +0000

    Terninary Operator

--- 16_ControlFlow/nullishOperator.js
+++ 16_ControlFlow/nullishOperator.js
@@ -7,3 +7,13 @@ let val1;
 val1 = null ?? 10 ?? 15
 
 console.log(val1)
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 60
+iceTeaPrice >= 80 ? console.log("less than 80") : console.log("More than 80")
+
+const isBeverage =  "coffee"
+isBeverage == "Tea" ? console.log("Let's Have Tea!") : console.log("Let's Have Coffee!")
