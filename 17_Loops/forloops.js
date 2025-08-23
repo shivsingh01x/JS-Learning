a7e6aae0b3b6b01bf41f4ee7042b70df2358e1a1
Shiv Singh Keer <shivamagrawal01x@gmail.com>
2025-08-23 02:39:46 +0000

    forloops

---
+++ 17_Loops/forloops.js
@@ -0,0 +1,20 @@
+// for of
+
+// ["", "", ""] - String in array
+// [{}, {}, {}] - object in arary
+
+// for of loop with array
+const myArr = [1, 2, 3, 4, 5]
+
+for (const element of myArr) {
+    // console.log(element);   
+}
+
+// for of loop with string
+
+const greetings = "Hello World!"
+for (const greet of greetings) {
+    console.log(`Each char is ${greet}`)
+}
+
+// video start form 6:20:29 video no:29 topic: MAP
