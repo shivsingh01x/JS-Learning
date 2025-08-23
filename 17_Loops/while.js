035c69c981849a67e2c14cc826b4fc22f71b7e18
Shiv Singh Keer <shivamagrawal01x@gmail.com>
2025-08-23 02:26:54 +0000

    do while loops

--- 17_Loops/while.js
+++ 17_Loops/while.js
@@ -12,8 +12,18 @@ let myArray = ["flash", "batman", "superman"]
 
 let arr = 0
 while (arr < myArray.length){
-    console.log(`value is ${myArray[arr]}`)
+    // console.log(`value is ${myArray[arr]}`)
     arr = arr + 1
 }
 
 // do while loop in js
+
+// let score = 0
+let score = 11
+
+do {
+    // console.log(`Score is ${score}`);
+    score++
+    
+} while (score <= 10);
+
